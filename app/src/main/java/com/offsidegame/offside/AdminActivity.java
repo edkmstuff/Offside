59746ebfd6083a967003865753fdfd235b7caede
Eran Dagan <eran.dagan1@gmail.com>
2016-11-15 01:04:57 +0200

    Added action menu to Admin Activity

--- app/src/main/java/com/offsidegame/offside/AdminActivity.java
+++ app/src/main/java/com/offsidegame/offside/AdminActivity.java
@@ -1,10 +1,13 @@
 package com.offsidegame.offside;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
@@ -24,6 +27,51 @@ public class AdminActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflates the menu; this addsitems to the action bar if it is exist
+        getMenuInflater().inflate(R.menu.admin_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean handled = true;
+        int id= item.getItemId();
+
+        switch (id){
+            case R.id.action_createGame:
+                onClickMenuShowCreateGame(item);
+                break;
+            case R.id.action_askQuestion:
+                onClickMenuShowAskQuestion(item);
+                break;
+            case R.id.action_answerQuestion:
+                onClickMenuShowAnswerQuestion(item);
+                break;
+            default:
+                handled = super.onOptionsItemSelected(item);
+        }
+
+        return handled;
+
+    }
+
+    void onClickMenuShowCreateGame (MenuItem item){
+        Intent intent = new Intent(this,CreateGameActivity.class);
+        startActivity(intent);
+
+    }
+    void onClickMenuShowAskQuestion (MenuItem item){
+        Intent intent = new Intent(this,AskQuestionActivity.class);
+        startActivity(intent);;
+    }
+
+    void onClickMenuShowAnswerQuestion (MenuItem item){
+        Intent intent = new Intent(this,AnswerQuestionActivity.class);
+        startActivity(intent);;
     }
 
 }
--- app/src/main/java/com/offsidegame/offside/CreateGameActivity.java
+++ app/src/main/java/com/offsidegame/offside/CreateGameActivity.java
@@ -2,6 +2,7 @@ package com.offsidegame.offside;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CreateGameActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class CreateGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
+    }
+
+    void ShowToast(String message){
+        Toast toast = Toast.makeText(this,message,Toast.LENGTH_LONG);
+
     }
 }
