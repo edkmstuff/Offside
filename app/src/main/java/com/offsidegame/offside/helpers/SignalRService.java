2adfc8746f91f4e0e953667bb27c64592ea3755f
edkmstuff <newcareer@123>
2016-12-26 23:13:56 +0200

    added login flow

--- app/src/main/java/com/offsidegame/offside/helpers/SignalRService.java
+++ app/src/main/java/com/offsidegame/offside/helpers/SignalRService.java
@@ -53,8 +53,8 @@ public class SignalRService extends Service {
     private final IBinder binder = new LocalBinder(); // Binder given to clients
 
     //public final String ip = new String("192.168.1.140:8080");
-    public final String ip = new String("10.0.0.17:8080");
-    //public final String ip = new String("offside.somee.com");
+    //public final String ip = new String("10.0.0.17:8080");
+    public final String ip = new String("offside.somee.com");
 
 
     //<editor-fold desc="constructors">
