999b4eb3227f1ed841850a6aa6275d811889e099
kfir <edkm.stuff@gmail.com>
2017-03-08 21:18:30 +0200

    before new login

--- app/src/main/java/com/offsidegame/offside/helpers/OffsideFirebaseMessagingService.java
+++ app/src/main/java/com/offsidegame/offside/helpers/OffsideFirebaseMessagingService.java
@@ -16,49 +16,49 @@ import java.net.Inet4Address;
 import java.util.Random;
 import java.util.UUID;
 
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
+//import com.google.firebase.messaging.FirebaseMessagingService;
+//import com.google.firebase.messaging.RemoteMessage;
 
 
 /**
  * Created by user on 12/26/2016.
  */
 
-public class OffsideFirebaseMessagingService extends FirebaseMessagingService {
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
-
-        String notificationBody="";
-        String notificationTitle= "";
-        String gameCode ="";
-        if (remoteMessage.getData().size() > 0) {
-            ArrayMap<String, String> data = (ArrayMap<String, String>) remoteMessage.getData();
-            notificationTitle = data.get("title").toString();
-            notificationBody = data.get("body").toString();
-            gameCode = remoteMessage.getData().get("gameCode").toString();
-
-            Intent intent = new Intent(this, LoginActivity.class);
-            intent.putExtra(getString(R.string.game_code_key),gameCode);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
-            //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, Intent.FILL_IN_ACTION);
-
-            notificationBuilder.setContentTitle(notificationTitle);
-            notificationBuilder.setContentText(notificationBody);
-            notificationBuilder.setAutoCancel(true);
-            notificationBuilder.setSmallIcon(R.mipmap.ic_offside_logo);
-
-            notificationBuilder.setContentIntent(pendingIntent);
-
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            Random r = new Random();
-            int notificationId = r.nextInt(100000 - 1) + 1;
-            notificationManager.notify(notificationId, notificationBuilder.build());
-        }
-
-    }
-}
+//public class OffsideFirebaseMessagingService extends FirebaseMessagingService {
+//
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        super.onMessageReceived(remoteMessage);
+//
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+//
+//        String notificationBody="";
+//        String notificationTitle= "";
+//        String gameCode ="";
+//        if (remoteMessage.getData().size() > 0) {
+//            ArrayMap<String, String> data = (ArrayMap<String, String>) remoteMessage.getData();
+//            notificationTitle = data.get("title").toString();
+//            notificationBody = data.get("body").toString();
+//            gameCode = remoteMessage.getData().get("gameCode").toString();
+//
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            intent.putExtra(getString(R.string.game_code_key),gameCode);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+//            //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, Intent.FILL_IN_ACTION);
+//
+//            notificationBuilder.setContentTitle(notificationTitle);
+//            notificationBuilder.setContentText(notificationBody);
+//            notificationBuilder.setAutoCancel(true);
+//            notificationBuilder.setSmallIcon(R.mipmap.ic_offside_logo);
+//
+//            notificationBuilder.setContentIntent(pendingIntent);
+//
+//            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//            Random r = new Random();
+//            int notificationId = r.nextInt(100000 - 1) + 1;
+//            notificationManager.notify(notificationId, notificationBuilder.build());
+//        }
+//
+//    }
+//}
