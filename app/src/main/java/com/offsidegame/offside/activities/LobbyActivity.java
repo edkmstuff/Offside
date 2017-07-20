12aeedf1f3c340ce61654c79046e27d19a4b5cdf
edkmstuff <newcareer@123>
2017-07-20 17:14:36 +0200

    added tabs to lobby - groups list shown in tabs

--- app/src/main/java/com/offsidegame/offside/activities/LobbyActivity.java
+++ app/src/main/java/com/offsidegame/offside/activities/LobbyActivity.java
@@ -26,7 +26,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.offsidegame.offside.R;
 import com.offsidegame.offside.adapters.GroupsPagerAdapterFragment;
-import com.offsidegame.offside.adapters.PrivateGroupAdapter;
+
 import com.offsidegame.offside.events.ActiveGameEvent;
 import com.offsidegame.offside.events.AvailableGamesEvent;
 import com.offsidegame.offside.events.AvailableLanguagesEvent;
@@ -50,7 +50,7 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+
 
 
 public class LobbyActivity extends AppCompatActivity implements Serializable {
@@ -58,58 +58,42 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
     //<editor-fold desc="REGION PROPERTIES">
 
 
-
+    //activity
     private final String activityName = "LobbyActivity";
     private final Context context = this;
     private final Activity thisActivity = this;
-    private EditText gameCodeEditText;
-    private TextView joinTextView;
-    private TextView userNameTextView;
-    private ImageView playerPictureImageView;
-    private LinearLayout joinGameRoot;
-    private LinearLayout loadingGameRoot;
-
-    private TextView createPrivateGameButtonTextView;
-    private LinearLayout createPrivateGameRoot;
-    private Spinner availableLanguagesSpinner;
-    private Spinner availableGamesSpinner;
-    private EditText privateGameNameEditText;
-    private TextView generatePrivateGameCodeButtonTextView;
-    private String[] gameTitles;
-    private String[] gameIds;
-    private String playerId;
-    private String playerDisplayName;
-    private String playerProfilePictureUrl;
-    private SharedPreferences settings;
-    private AvailableGame[] availableGames;
-    private String[] availableLanguages;
-    private TextView noAvailableGamesReturnLaterTextView;
-    private TextView versionTextView;
-    //private TextView playerBalanceTextView;
-////////////////////////////////////////////////////////
-
-
-    private TextView balanceTextView;
-    private TextView powerItemsTextView;
     private TextView createPrivateGroupButtonTextView;
-    private TextView savePrivateGroupButtonTextView;
-
-
     private LinearLayout privateGroupsRoot;
-
-
-
     private BottomNavigationView bottomNavigationView;
-
     private LinearLayout[] viewRoots = new LinearLayout[4];
-
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private TabLayout.OnTabSelectedListener listener;
 
+    //loading
+    private LinearLayout loadingGameRoot;
+    private TextView versionTextView;
+
+    //playerAssets
+    private TextView balanceTextView;
+    private TextView powerItemsTextView;
 
 
+    //profile
+    private TextView userNameTextView;
+    private ImageView playerPictureImageView;
+    private String playerId;
+    private String playerDisplayName;
+    private String playerProfilePictureUrl;
+    private SharedPreferences settings;
 
+    //create private group form
+    private LinearLayout createPrivateGroupFormRoot;
+    private Spinner availableLanguagesSpinner;
+    private EditText privateGroupNameEditText;
+    private String[] availableLanguages;
+    private TextView noAvailableGamesReturnLaterTextView;
+    private TextView savePrivateGroupButtonTextView;
     //</editor-fold>
 
 
@@ -159,27 +143,30 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
 
             //</editor-fold>
 
+
+            versionTextView = (TextView) findViewById(R.id.l_version_text_view);
+            versionTextView.setText(OffsideApplication.getVersion() == null ? "0.0" : OffsideApplication.getVersion());
+
+
             playerPictureImageView = (ImageView) findViewById(R.id.l_player_picture_image_view);
             balanceTextView = (TextView) findViewById(R.id.l_balance_text_view);
             powerItemsTextView = (TextView) findViewById(R.id.l_power_items_text_view);
             createPrivateGroupButtonTextView = (TextView) findViewById(R.id.l_create_private_group_button_text_view);
             savePrivateGroupButtonTextView = (TextView) findViewById(R.id.l_save_private_group_button_text_view);
-
             privateGroupsRoot = (LinearLayout) findViewById(R.id.l_private_groups_root);
-
-//            privateGroupsScrollView = (ScrollView) findViewById(R.id.l_private_groups_scroll_view);
-//            privateGroupsListView = (ListView) findViewById(R.id.l_private_groups_list_view);
 
             viewRoots[0] = (LinearLayout) findViewById(R.id.l_private_groups_root);
             viewRoots[1] = (LinearLayout) findViewById(R.id.l_profile_root);
             viewRoots[2] = (LinearLayout) findViewById(R.id.l_shop_root);
             viewRoots[3] = (LinearLayout) findViewById(R.id.l_play_root);
 
-            resetVisibility();
-            viewRoots[0].setVisibility(View.VISIBLE);
-
+            createPrivateGroupFormRoot = (LinearLayout) findViewById(R.id.l_create_private_group_form_root);
+            availableLanguagesSpinner = (Spinner) findViewById(R.id.l_available_language_spinner);
+            privateGroupNameEditText = (EditText) findViewById(R.id.l_private_game_name_edit_text);
             bottomNavigationView = (BottomNavigationView) findViewById(R.id.l_bottom_navigation_view);
 
+            resetVisibility();
+            viewRoots[0].setVisibility(View.VISIBLE);
 
             bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
@@ -210,8 +197,6 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
             });
 
 
-
-
 //            userNameTextView = (TextView) findViewById(R.id.jg_user_name_text_view);
 //            userNameTextView.setText(playerDisplayName);
 //
@@ -224,63 +209,42 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
 
 //
 
-//            joinTextView = (TextView) findViewById(R.id.jg_join_text_view);
-//
-//            joinTextView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    String gameCodeString = gameCodeEditText.getText().toString();
-//                    joinGame(gameCodeString, false);
-//                }
-//            });
-//
-//
-            versionTextView = (TextView) findViewById(R.id.l_version_text_view);
-            versionTextView.setText(OffsideApplication.getVersion() == null ? "0.0" : OffsideApplication.getVersion());
-//            joinGameRoot = (LinearLayout) findViewById(R.id.jg_join_game_root);
-//            loadingGameRoot = (LinearLayout) findViewById(R.id.jg_loading_root);
-            createPrivateGameRoot = (LinearLayout) findViewById(R.id.l_create_private_game_root);
-//
-//            createPrivateGameButtonTextView = (TextView) findViewById(R.id.jg_create_private_game_button_text_view);
-//
-//            availableLanguagesSpinner = (Spinner) findViewById(R.id.jg_available_language_spinner);
-//            availableGamesSpinner = (Spinner) findViewById(R.id.jg_available_games_spinner);
-//            privateGameNameEditText = (EditText) findViewById(R.id.jg_private_game_name_edit_text);
-//            generatePrivateGameCodeButtonTextView = (TextView) findViewById(R.id.jg_generate_private_game_code_button_text_view);
-//            noAvailableGamesReturnLaterTextView = (TextView) findViewById(R.id.jg_no_available_games_return_later_text_view);
-//            loadingGameRoot.setVisibility(View.VISIBLE);
-//            joinGameRoot.setVisibility(View.GONE);
-            createPrivateGameRoot.setVisibility(View.GONE);
-//            createPrivateGameButtonTextView.setVisibility(View.GONE);
-//            noAvailableGamesReturnLaterTextView.setVisibility(View.GONE);
-//
-//            createPrivateGameButtonTextView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//
-//                    privateGameNameEditText.setText(playerDisplayName.split(" ")[0] + "'s" + " friends");
-//                    joinGameRoot.setVisibility(View.GONE);
-//                    createPrivateGameRoot.setVisibility(View.VISIBLE);
-//                }
-//            });
-//
+
+
+
             createPrivateGroupButtonTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+                    for (LinearLayout layout : viewRoots) {
+                        layout.setVisibility(View.GONE);
+
+                    }
+
+                    privateGroupNameEditText.setText(playerDisplayName.split(" ")[0] + "'s" + " friends");
+                    createPrivateGroupButtonTextView.setVisibility(View.GONE);
+                    createPrivateGroupFormRoot.setVisibility(View.VISIBLE);
+                }
+            });
+//
+            savePrivateGroupButtonTextView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     //get language
                     String selectedLanguage = availableLanguagesSpinner.getSelectedItem().toString();
 
-                    String groupName = privateGameNameEditText.getText().toString();
+                    String groupName = privateGroupNameEditText.getText().toString();
                     groupName = groupName.length() > 20 ? groupName.substring(0, 20) : groupName;
 
                     //todo: change this call to a method that create the group only , no game entry is required
 
                     if (OffsideApplication.isBoundToSignalRService)
-                        OffsideApplication.signalRService.createPrivateGroup( groupName, playerId, selectedLanguage);
+                        OffsideApplication.signalRService.createPrivateGroup(groupName, playerId, selectedLanguage);
                     else
                         throw new RuntimeException(activityName + " - generatePrivateGameCodeButtonTextView - onClick - Error: SignalRIsNotBound");
 
-                    createPrivateGameRoot.setVisibility(View.GONE);
+                    createPrivateGroupFormRoot.setVisibility(View.GONE);
                     privateGroupsRoot.setVisibility(View.VISIBLE);
                 }
             });
@@ -298,8 +262,8 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
     private void resetVisibility() {
 
         privateGroupsRoot.setVisibility(View.VISIBLE);
-        createPrivateGameRoot.setVisibility(View.VISIBLE);
-        createPrivateGameButtonTextView.setVisibility(View.VISIBLE);
+        createPrivateGroupButtonTextView.setVisibility(View.VISIBLE);
+        createPrivateGroupFormRoot.setVisibility(View.GONE);
 
         for (LinearLayout layout : viewRoots) {
             layout.setVisibility(View.GONE);
@@ -331,23 +295,21 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
     }
 
 
-    private void joinGame(String privateGameCode, boolean isPrivateGameCreator) {
-        if (OffsideApplication.isBoundToSignalRService) {
+//    private void joinGame(String privateGameCode, boolean isPrivateGameCreator) {
+//        if (OffsideApplication.isBoundToSignalRService) {
+//
+//            OffsideApplication.setIsPlayerQuitGame(false);
+//            String androidDeviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+//
+//
+//            OffsideApplication.signalRService.joinGame(privateGameCode, playerId, playerDisplayName, playerProfilePictureUrl, isPrivateGameCreator, androidDeviceId);
+//            loadingGameRoot.setVisibility(View.VISIBLE);
+//            joinGameRoot.setVisibility(View.GONE);
+//            createPrivateGroupFormRoot.setVisibility(View.GONE);
+//        }
+//    }
 
-            OffsideApplication.setIsPlayerQuitGame(false);
-            String androidDeviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
 
-
-            OffsideApplication.signalRService.joinGame(privateGameCode, playerId, playerDisplayName, playerProfilePictureUrl, isPrivateGameCreator, androidDeviceId);
-            loadingGameRoot.setVisibility(View.VISIBLE);
-            joinGameRoot.setVisibility(View.GONE);
-            createPrivateGameRoot.setVisibility(View.GONE);
-        }
-    }
-
-    private String getGameId(int selectedGamePosition) {
-        return gameIds[selectedGamePosition];
-    }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onSignalRServiceBinding(SignalRServiceBoundEvent signalRServiceBoundEvent) {
@@ -404,68 +366,9 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
         }
     }
 
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onJoinGame(JoinGameEvent joinGameEvent) {
-        try {
 
 
-            GameInfo gameInfo = joinGameEvent.getGameInfo();
-            if (gameInfo == null) {
-                Toast.makeText(context, R.string.lbl_no_such_game, Toast.LENGTH_LONG).show();
-                loadingGameRoot.setVisibility(View.GONE);
-                joinGameRoot.setVisibility(View.VISIBLE);
-                return;
-            }
-            String gameId = gameInfo.getGameId();
-            String gameCode = gameInfo.getPrivateGameCode();
-            String privateGameTitle = gameInfo.getPrivateGameTitle();
-            String homeTeam = gameInfo.getHomeTeam();
-            String awayTeam = gameInfo.getAwayTeam();
 
-            OffsideApplication.setGameInfo(gameInfo);
-//            int offsideCoins = player != null? player.getOffsideCoins() : 0;
-
-
-            SharedPreferences.Editor editor = settings.edit();
-
-            editor.putString(getString(R.string.game_id_key), gameId);
-            editor.putString(getString(R.string.game_code_key), gameCode);
-            editor.putString(getString(R.string.private_game_title_key), privateGameTitle);
-            editor.putString(getString(R.string.home_team_key), homeTeam);
-            editor.putString(getString(R.string.away_team_key), awayTeam);
-
-
-            editor.commit();
-
-            Intent intent = new Intent(context, ChatActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-            startActivity(intent);
-        } catch (Exception ex) {
-            ACRA.getErrorReporter().handleSilentException(ex);
-        }
-    }
-
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onReceiveIsGameActive(ActiveGameEvent activeGameEvent) {
-        try {
-            Boolean isGameActive = activeGameEvent.getIsGameActive();
-
-            if (isGameActive) {
-                //Intent intent = new Intent(context, ViewPlayerScoreActivity.class);
-                SharedPreferences settings = getSharedPreferences(getString(R.string.preference_name), 0);
-                String gameCode = settings.getString(getString(R.string.game_code_key), "");
-                joinGame(gameCode, false);
-
-            } else {
-                loadingGameRoot.setVisibility(View.GONE);
-                joinGameRoot.setVisibility(View.VISIBLE);
-            }
-        } catch (Exception ex) {
-            ACRA.getErrorReporter().handleSilentException(ex);
-        }
-    }
 
 
     @Subscribe(threadMode = ThreadMode.MAIN)
@@ -481,30 +384,7 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
     }
 
 
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void onReceiveAvailableGames(AvailableGamesEvent availableGamesEvent) {
-        try {
-            availableGames = availableGamesEvent.getAvailableGames();
-            if (availableGames.length == 0) {
-                noAvailableGamesReturnLaterTextView.setVisibility(View.VISIBLE);
-                throw new Exception(activityName + " - onReceiveAvailableGames - Error: available games is empty ");
-            }
 
-            createPrivateGameButtonTextView.setVisibility(View.VISIBLE);
-            noAvailableGamesReturnLaterTextView.setVisibility(View.GONE);
-
-            gameTitles = new String[availableGames.length];
-            gameIds = new String[availableGames.length];
-            for (int i = 0; i < availableGames.length; i++) {
-                gameIds[i] = availableGames[i].getGameId();
-                gameTitles[i] = availableGames[i].getGameTitle();
-            }
-
-            setAvailableGamesSpinnerAdapter(gameTitles);
-        } catch (Exception ex) {
-            ACRA.getErrorReporter().handleSilentException(ex);
-        }
-    }
 
     private void setAvailableLanguageSpinnerAdapter(String[] languages) {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, languages);
@@ -516,26 +396,11 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
             availableLanguagesSpinner.setSelection(0);
     }
 
-    private void setAvailableGamesSpinnerAdapter(String[] gameTitles) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, gameTitles);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        availableGamesSpinner.setAdapter(adapter);
-        if (gameTitles.length > 0)
-            availableGamesSpinner.setSelection(0);
-    }
-
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onPrivateGameGenerated(PrivateGameGeneratedEvent privateGameGeneratedEvent) {
         try {
-            String privateGameCode = privateGameGeneratedEvent.getPrivateGameCode();
-            joinGame(privateGameCode, true);
-            //privateGameCodeTextView.setText(privateGameCode);
-            //gameCodeEditText.setText(privateGameCode);
-            //privateGameCodeTextView.setVisibility(View.VISIBLE);
-
+        //todo:update GameInfo
         } catch (Exception ex) {
             ACRA.getErrorReporter().handleSilentException(ex);
         }
@@ -590,9 +455,7 @@ public class LobbyActivity extends AppCompatActivity implements Serializable {
     @Override
     public void onBackPressed() {
 
-        joinGameRoot.setVisibility(View.VISIBLE);
-        createPrivateGameRoot.setVisibility(View.GONE);
-
+        resetVisibility();
         if (exit) {
             finish(); // finish activity
         } else {
